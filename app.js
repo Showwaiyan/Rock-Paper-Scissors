0015a40e9e33223a3346fc1cb8d2fbfd36ded561
Show WaiYan <showwaiyan555@gmail.com>
2022-06-26 22:19:00 +0630

    Fixing bug of draw result

--- app.js
+++ app.js
@@ -20,58 +20,32 @@ function playRound(playerSelection, computerSelection) {
     switch (playerSelection) {
         case ("rock"):
             if (computerSelection === "paper") {
-                console.log("You Lose! Paper beats Rock");
                 return false;
             }
             else {
-                console.log("You Win! Rock beats Scissors");
                 return true;
             }
 
         case ("paper"):
             if (computerSelection === "rock") {
-                console.log("You Win! Paper beats Rock");
                 return true;
             }  
             else {
-                console.log("You Lose! Scissors beats Paper");
                 return false;
             } 
 
         case ("scissors"):
             if (computerSelection === "paper") {
-                console.log("You Win! Scissors beats Paper");
                 return true;
             }  
             else {
-                console.log("You Lose! Rocks beats Scissors");
                 return false;
             }
     }
 }
 
-function game() {
-    // Main loop of the game
-    let gamestate = 0
-    for (let i = 0; i < 5; i++) {
-        let PlayerSelection = window.prompt("What is your choice : ");
-        let ComputerSelection = computerPlay();
-
-        if (PlayerSelection === ComputerSelection) {
-            // Test the two input is same to be draw round
-            console.log("Draw! Play Again");
-            continue;
-        }
-
-        if (playRound(PlayerSelection.toLowerCase(), ComputerSelection.toLowerCase())) {
-            gamestate++;
-        }
-        else gamestate--;
-    } 
-    console.log(gamestate > 0 ? "You Win!" : gamestate == 0 ? "Draw!" : "Computer Win!");
-}
-
 const selector = document.querySelector('.selection');
+
 let PlayerSelection;
 let PlayerMark = 0;
 
@@ -89,8 +63,9 @@ selector.addEventListener('click',(e)=>{
 
         human.src = `images/${e.target.parentElement.dataset.card}.png`;
         computer.src = `images/${ComputerSelection}.png`;
-
-        if (playRound(PlayerSelection,ComputerSelection)) PlayerMark++;
+        
+        if (PlayerSelection === ComputerSelection) return;
+        else if (playRound(PlayerSelection,ComputerSelection)) PlayerMark++;
         else ComputerMark++;
 
         document.getElementById("marks").textContent = `${PlayerMark}  :  ${ComputerMark}`;
